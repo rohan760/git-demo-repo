8d696b0cf5e773f6f6cd0e2271a298ef1c0885e2
rohan760 <69039507+rohan760@users.noreply.github.com>
2022-02-10 19:43:50 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#add implementation
+def add(x,y):
+    pass
+#subtruct implementation
+def subtruct(x,y):
+    pass
+#multiply implementation
+def multiply(x,y):
+    pass
+#divide implementation
+def divide(x,y):
+    pass
